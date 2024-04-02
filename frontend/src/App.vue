4f28d4b69ff86f26731a1af5bd31026bdd705462
FernandaOsorioMorales <fernanda_osorio@ciencias.unam.mx>
2024-04-02 01:04:10 -0600

    Landing page en construcción

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,29 @@
 <template>
-  <button class="btn">Hello daisyui</button>
+  <div id="app">
+    <landingpage/>
+  </div>
 </template>
+
+<script>
+import landingpage from './components/landingpage.vue';
+
+export default {
+  name: 'App',
+  components: {
+    landingpage
+  }
+}
+</script>
+
+<style>
+*, *::before, *::after {
+  font-family: 'Sen', sans-serif;
+}
+#app {
+  background-color: #F6DBEA;  
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,28 @@
 module.exports = {
     content: ['./src/**/*.{vue,js,ts}'],
     plugins: [require('daisyui')],
+    daisyui: {
+      themes: [
+        {
+          fer: {
+            "primary":"#BE3E82",
+            "secondary":"#E43F6F",
+            "accent":"#5E4352",
+            "neutral":"#DFBBB1",
+            "otroRosa":"#EA5DBA",
+            "base-100":"ffffff",
+          },
+          extend: {
+            fontFamily: {
+              'sen': ['Sen', 'sans-serif'],
+            },
+            fontOpticalSizing: {
+              'auto': 'auto',
+            },
+          },
+        },
+        "dark",
+        "cupcake",
+      ]
+    }
   };
